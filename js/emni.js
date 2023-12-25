05a2874d4ca39a6cf49f8ea2835ecbed506523e8
Omar Faruk Um <org.omar.faruk@gmail.com>
2023-12-25 15:21:25 +0600

    first commit

---
+++ js/emni.js
@@ -0,0 +1,15 @@
+function Omar(){
+    const txt = document.getElementById('id1')
+    txt.innerHTML = `হ্যাঁ স্বামী, এই যে আমি।`
+}
+
+function Faruk()
+{
+    setTimeout(myFunction, 3000);
+
+function myFunction() {
+    const txt = document.getElementById('id2')
+    txt.innerHTML = `আজকে ছুটির দিন, ল্যাপটপ নিয়ে বসেই ছিলাম। ভাবলাম একটু বউ কে ডাকি🤣🤣🤣`
+    myFunction();
+}
+}
